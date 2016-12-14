56aec6e62f2e2f0ea283a95fa15d6298c0dbe772
hdupont <dupont.hubert@gmail.com>
2016-12-14 07:25:58 +0100

    Doc

--- dependencies/h_parsetk.js
+++ dependencies/h_parsetk.js
@@ -62,6 +62,18 @@ var h_parsetk = (function() {
 			}
 			return token;
 		},
+		/**
+		 * Retourne l'indice du premier caractère du index-ième token trouvé à
+		 * partir du charactère d'indice start, avec index commençant à 1 et
+		 * start commençant à zéro.
+		 * @param {string} str La chaine dans laquelle s'effectue la recherche.
+		 * @param {int} index Le numéro (commençant à 1) du token qu'on souhaite.
+		 * @param {int} start L'indice du caractère à partir duquel effectuer
+		 * la recherche.
+		 * @returns {int} L'indice du premier caractère du token cherché.
+		 * 
+		 * TODO Uniformiser l'interface. Tout le monde commence à 1 ou à zéro.
+		 */
 		findTokenIndex: function(str, index, start) {
 			var nonSpace = start;
 			var space = start;
@@ -73,11 +85,42 @@ var h_parsetk = (function() {
 			var firstTokenCharIndex = nonSpace;
 			return firstTokenCharIndex;
 		},
+		/**
+		 * Retourne l'index-ième token trouvé à partir du charactère d'indice start,
+		 * avec index commençant à 1 et start commençant à zéro.
+		 * @param {string} str La chaine dans laquelle s'effectue la recherche.
+		 * @param {int} index Le numéro (commençant à 1) du token qu'on souhaite.
+		 * @param {int} start L'indice du caractère à partir duquel effectuer
+		 * la recherche.
+		 * @returns {string} L'index-ième token trouvé à partir de start. 
+		 * 
+		 * EXEMPLE Soit la chaine
+		 * Lorem ipsum dolor sit amet
+		 * 01234567890123456789012345 <- indice des caractères (commence à 0)
+		 * 
+		 * 1. Le premier token trouvé à partir du caractère d'indice 7
+		 * findToken: function("Lorem ipsum dolor sit amet", 1, 7)
+		 * => "psum"
+		 *
+		 * 2. Le deuxième token trouvé à partir du caractère d'indice 7
+		 * findToken: function("Lorem ipsum dolor sit amet", 2, 7)
+		 * => "dolor"
+		 * 
+		 * TODO Uniformiser l'interface. Tout le monde commence à 1 ou à zéro.
+		 */
 		findToken: function(str, index, start) {
 			var firstTokenCharIndex = this.findTokenIndex(str, index, start);
 			var token = this.peekToken(str, firstTokenCharIndex);
 			return token;
 		},
+		/**
+		 * Retourne la partie de la chaine str commençant après l'index-ième token.
+		 * @param {string} str La chaine dans laquelle va s'effectuer la recherche.
+		 * @param {index} Le numéro (commençant à 1) du token après lequel on
+		 * souhaite que la chaine retournée commence.
+		 * @returns {string} La partie de la chaine str commençant après
+		 * l'index-ième token.
+		 */
 		peekAfterToken: function(str, index) {
 			var firstTokenCharIndex = this.findTokenIndex(str, index + 1, 0);
 			return str.slice(firstTokenCharIndex);
